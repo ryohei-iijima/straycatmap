9295424bd1f76d4ab41e2c376ebd2d013cd3a91d
RyoheIiijima <ryoheiiijima@RyoheIiijimanoMacBook-Pro.local>
2024-01-08 21:15:40 +0900

    コンフィグ修正

--- next.config.js
+++ next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['firebasestorage.googleapis.com'],
-  }
+  },
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
