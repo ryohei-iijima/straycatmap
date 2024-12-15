0de500bccb4721ee12ac6eaa053bd7cb966f074c
RyoheIiijima <ryoheiiijima@RyoheIiijimanoMacBook-Pro.local>
2024-12-15 14:04:30 +0900

    ビルド時のエラー解消

--- features/GoogleMapComponents/GoogleMapComponents.tsx
+++ features/GoogleMapComponents/GoogleMapComponents.tsx
@@ -5,10 +5,8 @@ import getCatMapInfo from 'features/CatMap/api/getCatMapInfo';
 import { catMapInfos, catMapInfo } from 'features/Top/types';
 import styles from './GoogleMapComponents.module.scss';
 
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-  key: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY,
-  v: "weekly",
-});
+// @ts-ignore
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({key: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY,v: "weekly",});
 
 const mapMarker = (imageURL: string) => {
   const parser = new DOMParser();
